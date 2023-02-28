8179af8366060df014259839a27edec2ceea407b
Aimen Moten <aimen.moten@gmail.com>
2023-02-27 23:13:50 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var numOfButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numOfButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keydown", function(Event){
+    makeSound(Event.key);
+    buttonAnimation(Event.key);
+})
+
+function makeSound(key){
+    switch (key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+        break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+        break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+        break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+        break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+        break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+        break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+        break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(key){
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
